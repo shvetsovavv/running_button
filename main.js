6211771530798440bee205bda6033ab86f376f90
Vika Shvetsova <shvetsova.vv.97@gmail.com>
2023-12-03 00:41:21 +0300

    improve

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 
 function main() {
     const button_no = document.querySelector(".button_no");
-    button_no.addEventListener("mouseover", () => {
-        console.log(button_no.style);
-        if (button_no.style.gridArea == "E / E / E / E") {
-            button_no.style.gridArea = "B";
-        } else {
-            button_no.style.gridArea = "E";
-        }
-    });
+
+    button_no.addEventListener("mouseover", changeGrid);
+    button_no.addEventListener("click", changeGrid);
 
     const button_yes = document.querySelector(".button_yes");
     button_yes.addEventListener("click", () => {
         alert("I love you too!");
     });
+
+    function changeGrid() {
+        if (button_no.style.gridArea == "E / E / E / E") {
+            button_no.style.gridArea = "B";
+        } else {
+            button_no.style.gridArea = "E";
+        }
+    };
 }
 
 main();
